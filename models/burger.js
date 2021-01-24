63e1e337bffa6bbdbd20bf08db0b981f35aaab23
pjdip <pjdip1@gmail.com>
2021-01-24 18:40:40 -0500

    burger model first function

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+let orm = require("../config/orm");
+
+let burger = {
+    selectAll: callback => {
+        orm.selectAll("burgers", result => {
+            callback(result);
+        });
+    },
+    
+}
