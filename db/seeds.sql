111d59b57ddc328c8eba26595b1bad65f6e21757
pjdip <pjdip1@gmail.com>
2021-01-23 22:04:43 -0500

    seeds.sql with 3 burgers

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Barbecue Chicken Burger");
