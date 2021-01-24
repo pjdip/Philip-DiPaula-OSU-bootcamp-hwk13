ea51b4b5b485df9273c83a2b1e51efa9c27bd2e4
pjdip <pjdip1@gmail.com>
2021-01-23 21:46:20 -0500

    handlebars and express setup in server with middleware and routing connection to controller

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = require('express');
+// Dependencies
+const express = require("express");
+
+//HandleBars Things
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let PORT = process.env.PORT || 8080;
+let app = express();
+
+//Express Things
+// Serve static content for the app from the "public" directory in the application directory
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Import routes to the server
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
